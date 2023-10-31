ec93dc538b975ac986e1a1039dc1f4bbfc230b09
Muhammed Aslam K P <147359788+Aslamkp2004@users.noreply.github.com>
2023-10-31 15:43:14 +0530

    Mini-Max Sum.c

---
+++ c/Mini-Max Sum.c
@@ -0,0 +1,186 @@
+#include <assert.h>
+#include <ctype.h>
+#include <limits.h>
+#include <math.h>
+#include <stdbool.h>
+#include <stddef.h>
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+char* readline();
+char* ltrim(char*);
+char* rtrim(char*);
+char** split_string(char*);
+
+int parse_int(char*);
+
+/*
+ * Complete the 'miniMaxSum' function below.
+ *
+ * The function accepts INTEGER_ARRAY arr as parameter.
+ */
+
+void miniMaxSum(int arr_count, int* arr) {
+    long long minSum = arr[0];  // Initialize minSum and maxSum with the first element.
+    long long maxSum = arr[0];
+    long long totalSum = arr[0];  // Initialize the totalSum with the first element.
+
+    // Find the minimum and maximum sums and calculate the totalSum.
+    for (int i = 1; i < arr_count; i++) {
+        totalSum += arr[i];
+        if (arr[i] < minSum) {
+            minSum = arr[i];
+        }
+        if (arr[i] > maxSum) {
+            maxSum = arr[i];
+        }
+    }
+
+    // Calculate the minimum sum (totalSum - max element) and the maximum sum (totalSum - min element).
+    long long minSumResult = totalSum - maxSum;
+    long long maxSumResult = totalSum - minSum;
+
+    // Print the minimum and maximum sums.
+    printf("%lld %lld\n", minSumResult, maxSumResult);
+}
+
+
+int main()
+{
+
+    char** arr_temp = split_string(rtrim(readline()));
+
+    int* arr = malloc(5 * sizeof(int));
+
+    for (int i = 0; i < 5; i++) {
+        int arr_item = parse_int(*(arr_temp + i));
+
+        *(arr + i) = arr_item;
+    }
+
+    miniMaxSum(5, arr);
+
+    return 0;
+}
+
+char* readline() {
+    size_t alloc_length = 1024;
+    size_t data_length = 0;
+
+    char* data = malloc(alloc_length);
+
+    while (true) {
+        char* cursor = data + data_length;
+        char* line = fgets(cursor, alloc_length - data_length, stdin);
+
+        if (!line) {
+            break;
+        }
+
+        data_length += strlen(cursor);
+
+        if (data_length < alloc_length - 1 || data[data_length - 1] == '\n') {
+            break;
+        }
+
+        alloc_length <<= 1;
+
+        data = realloc(data, alloc_length);
+
+        if (!data) {
+            data = '\0';
+
+            break;
+        }
+    }
+
+    if (data[data_length - 1] == '\n') {
+        data[data_length - 1] = '\0';
+
+        data = realloc(data, data_length);
+
+        if (!data) {
+            data = '\0';
+        }
+    } else {
+        data = realloc(data, data_length + 1);
+
+        if (!data) {
+            data = '\0';
+        } else {
+            data[data_length] = '\0';
+        }
+    }
+
+    return data;
+}
+
+char* ltrim(char* str) {
+    if (!str) {
+        return '\0';
+    }
+
+    if (!*str) {
+        return str;
+    }
+
+    while (*str != '\0' && isspace(*str)) {
+        str++;
+    }
+
+    return str;
+}
+
+char* rtrim(char* str) {
+    if (!str) {
+        return '\0';
+    }
+
+    if (!*str) {
+        return str;
+    }
+
+    char* end = str + strlen(str) - 1;
+
+    while (end >= str && isspace(*end)) {
+        end--;
+    }
+
+    *(end + 1) = '\0';
+
+    return str;
+}
+
+char** split_string(char* str) {
+    char** splits = NULL;
+    char* token = strtok(str, " ");
+
+    int spaces = 0;
+
+    while (token) {
+        splits = realloc(splits, sizeof(char*) * ++spaces);
+
+        if (!splits) {
+            return splits;
+        }
+
+        splits[spaces - 1] = token;
+
+        token = strtok(NULL, " ");
+    }
+
+    return splits;
+}
+
+int parse_int(char* str) {
+    char* endptr;
+    int value = strtol(str, &endptr, 10);
+
+    if (endptr == str || *endptr != '\0') {
+        exit(EXIT_FAILURE);
+    }
+
+    return value;
+}
